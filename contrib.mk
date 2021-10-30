6dbe63dd3d48fa51b519cfb458991eb50f2acd4f
Koichi Murase <myoga.murase@gmail.com>
2021-10-30 15:50:06 +0900

    contrib.mk: add a workaround for make-3.81

--- contrib.mk
+++ contrib.mk
@@ -12,5 +12,11 @@ outdirs += $(OUTDIR)/doc/contrib
 outfiles-doc += $(OUTDIR)/doc/contrib/LICENSE
 outfiles-doc += $(OUTDIR)/doc/contrib/README-ja.md
 outfiles-doc += $(OUTDIR)/doc/contrib/README.md
-$(OUTDIR)/doc/contrib/%: contrib/% | $(OUTDIR)/doc/contrib
+
+# Note (workaround for make-3.81): 当初 $(OUTDIR)/doc/contrib/% に対してルール
+# を記述していたが make-3.81 に於いて正しく適用されない事が分かった。仕方がない
+# ので LICENSE と %.md の二つの規則に分けて書く事にする。
+$(OUTDIR)/doc/contrib/LICENSE: contrib/LICENSE | $(OUTDIR)/doc/contrib
+	cp -p $< $@
+$(OUTDIR)/doc/contrib/%.md: contrib/%.md | $(OUTDIR)/doc/contrib
 	cp -p $< $@
