ac044d215c45045f17583978418ee471a8f66a06
Koichi Murase <myoga.murase@gmail.com>
2023-06-30 04:59:11 +0900

    fzf-git: sync settings with the gist upstream 2022-08-16

--- integration/fzf-git.bash
+++ integration/fzf-git.bash
@@ -1,5 +1,5 @@
 # ble/contrib/integration/fzf-git.bash (C) 2020, 2023, akinomyoga
-# Ported from https://gist.github.com/junegunn/8b572b8d4b5eddd8b85e5f4d40f17236
+# Ported from https://gist.github.com/junegunn/8b572b8d4b5eddd8b85e5f4d40f17236 (Revision 2022-08-16)
 
 ble-import contrib/integration/fzf-initialize
 
@@ -13,18 +13,18 @@ is_in_git_repo() {
 }
 
 fzf-down() {
-  fzf --height 50% "$@" --border
+  fzf --height 50% --min-height 20 --border --bind ctrl-/:toggle-preview "$@"
 }
 
-gf() {
+_gf() {
   is_in_git_repo || return 1
   git -c color.status=always status --short |
     fzf-down -m --ansi --nth 2..,.. \
-             --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
+             --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1})' |
     cut -c4- | sed 's/.* -> //'
 }
 
-gb() {
+_gb() {
   is_in_git_repo || return 1
   git branch -a --color=always | grep -v '/HEAD\s' | sort |
     fzf-down --ansi --multi --tac --preview-window right:70% \
@@ -33,28 +33,34 @@ gb() {
     sed 's#^remotes/##'
 }
 
-gt() {
+_gt() {
   is_in_git_repo || return 1
   git tag --sort -version:refname |
     fzf-down --multi --preview-window right:70% \
-             --preview 'git show --color=always {} | head -'$LINES
+             --preview 'git show --color=always {}'
 }
 
-gh() {
+_gh() {
   is_in_git_repo || return 1
   git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
     fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
              --header 'Press CTRL-S to toggle sort' \
-             --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always | head -'$LINES |
+             --preview 'grep -o "[a-f0-9]\{7,\}" <<< {} | xargs git show --color=always' |
     grep -o "[a-f0-9]\{7,\}"
 }
 
-gr() {
+_gr() {
   is_in_git_repo || return 1
   git remote -v | awk '{print $1 "\t" $2}' | uniq |
     fzf-down --tac \
-             --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
+             --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1}' |
     cut -d$'\t' -f1
+}
+
+_gs() {
+  is_in_git_repo || return
+  git stash list | fzf-down --reverse -d: --preview 'git show --color=always {1}' |
+  cut -d: -f1
 }
 
 # export FZF_DEFAULT_OPTS=--no-unicode
@@ -63,22 +69,24 @@ gr() {
 
 if [[ :$_ble_contrib_fzf_git_config: == *:original:* ]]; then
   bind '"\er": redraw-current-line'
-  bind '"\C-g\C-f": "$(gf)\e\C-e\er"'
-  bind '"\C-g\C-b": "$(gb)\e\C-e\er"'
-  bind '"\C-g\C-t": "$(gt)\e\C-e\er"'
-  bind '"\C-g\C-h": "$(gh)\e\C-e\er"'
-  bind '"\C-g\C-r": "$(gr)\e\C-e\er"'
+  bind '"\C-g\C-f": "$(_gf)\e\C-e\er"'
+  bind '"\C-g\C-b": "$(_gb)\e\C-e\er"'
+  bind '"\C-g\C-t": "$(_gt)\e\C-e\er"'
+  bind '"\C-g\C-h": "$(_gh)\e\C-e\er"'
+  bind '"\C-g\C-r": "$(_gr)\e\C-e\er"'
+  bind '"\C-g\C-s": "$(_gs)\e\C-e\er"'
 
-  # bind '"\C-g\C-f": "$(gf)\M-\C-e\M-\C-l"'
-  # bind '"\C-g\C-b": "$(gb)\M-\C-e\M-\C-l"'
-  # bind '"\C-g\C-t": "$(gt)\M-\C-e\M-\C-l"'
-  # bind '"\C-g\C-h": "$(gh)\M-\C-e\M-\C-l"'
-  # bind '"\C-g\C-r": "$(gr)\M-\C-e\M-\C-l"'
+  # bind '"\C-g\C-f": "$(_gf)\M-\C-e\M-\C-l"'
+  # bind '"\C-g\C-b": "$(_gb)\M-\C-e\M-\C-l"'
+  # bind '"\C-g\C-t": "$(_gt)\M-\C-e\M-\C-l"'
+  # bind '"\C-g\C-h": "$(_gh)\M-\C-e\M-\C-l"'
+  # bind '"\C-g\C-r": "$(_gr)\M-\C-e\M-\C-l"'
+  # bind '"\C-g\C-s": "$(_gs)\M-\C-e\M-\C-l"'
 fi
 
 if [[ :$_ble_contrib_fzf_git_config: == *:key-binding:* ]]; then
   function ble/widget/fzf-git {
-    ble/widget/insert-string "$($1)"
+    ble/widget/insert-string "$(_$1)"
     ble/textarea#invalidate
   }
   ble-bind -f 'C-g C-f' 'fzf-git gf'
@@ -86,11 +94,12 @@ if [[ :$_ble_contrib_fzf_git_config: == *:key-binding:* ]]; then
   ble-bind -f 'C-g C-t' 'fzf-git gt'
   ble-bind -f 'C-g C-h' 'fzf-git gh'
   ble-bind -f 'C-g C-r' 'fzf-git gr'
+  ble-bind -f 'C-g C-s' 'fzf-git gs'
 fi
 
 if [[ :$_ble_contrib_fzf_git_config: == *:sabbrev:* ]]; then
   function fzf-git.sabbrev {
-    COMPREPLY=$($1)
+    COMPREPLY=$(_$1)
     ble/textarea#invalidate
   }
   ble-sabbrev -m gf='fzf-git.sabbrev gf'
@@ -98,6 +107,7 @@ if [[ :$_ble_contrib_fzf_git_config: == *:sabbrev:* ]]; then
   ble-sabbrev -m gt='fzf-git.sabbrev gt'
   ble-sabbrev -m gh='fzf-git.sabbrev gh'
   ble-sabbrev -m gr='fzf-git.sabbrev gr'
+  ble-sabbrev -m gs='fzf-git.sabbrev gs'
 fi
 
 if [[ :$_ble_contrib_fzf_git_config: == *:arpeggio:* ]]; then
@@ -107,4 +117,5 @@ if [[ :$_ble_contrib_fzf_git_config: == *:arpeggio:* ]]; then
   ble/lib/vim-arpeggio.sh/bind -f 'gt' 'fzf-git gt'
   ble/lib/vim-arpeggio.sh/bind -f 'gh' 'fzf-git gh'
   ble/lib/vim-arpeggio.sh/bind -f 'gr' 'fzf-git gr'
+  ble/lib/vim-arpeggio.sh/bind -f 'gs' 'fzf-git gs'
 fi
