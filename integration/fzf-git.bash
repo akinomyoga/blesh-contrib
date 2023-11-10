1ec7468f320a82efe9d7ee4d275d365f225aa014
Koichi Murase <myoga.murase@gmail.com>
2023-11-10 18:19:21 +0900

    integration/fzf-git: add command mode used by some keybindings

--- integration/fzf-git.bash
+++ integration/fzf-git.bash
@@ -7,6 +7,87 @@
 # 2023-06-30 https://github.com/junegunn/fzf-git.sh/commit/b6192ec86609afea761c7d3954f9b539a512dc80
 # 2023-11-09 https://github.com/junegunn/fzf-git.sh/blob/aacab4ae557657e0f9de288d688f312a28b86d21/fzf-git.sh
 
+if [[ $- != *i* ]]; then
+  if (($# == 1)); then
+    function ble/contrib/integration:fzf-git/sub:branches {
+      git branch "$@" --sort=-committerdate --sort=-HEAD --format=$'%(HEAD) %(color:yellow)%(refname:short) %(color:green)(%(committerdate:relative))\t%(color:blue)%(subject)%(color:reset)' --color=always | column -ts$'\t'
+    }
+    function ble/contrib/integration:fzf-git/sub:refs {
+      git for-each-ref --sort=-creatordate --sort=-HEAD --color=always --format=$'%(refname) %(color:green)(%(creatordate:relative))\t%(color:blue)%(subject)%(color:reset)' |
+        eval "$1" |
+        sed 's#^refs/remotes/#\x1b[95mremote-branch\t\x1b[33m#; s#^refs/heads/#\x1b[92mbranch\t\x1b[33m#; s#^refs/tags/#\x1b[96mtag\t\x1b[33m#; s#refs/stash#\x1b[91mstash\t\x1b[33mrefs/stash#' |
+        column -ts$'\t'
+    }
+    case $1 in
+    (branches)
+      echo $'CTRL-O (open in browser) ╱ ALT-A (show all branches)\n'
+      ble/contrib/integration:fzf-git/sub:branches
+      ;;
+    (all-branches)
+      echo $'CTRL-O (open in browser)\n'
+      ble/contrib/integration:fzf-git/sub:branches -a
+      ;;
+    (refs)
+      echo $'CTRL-O (open in browser) ╱ ALT-E (examine in editor) ╱ ALT-A (show all refs)\n'
+      ble/contrib/integration:fzf-git/sub:refs 'grep -v ^refs/remotes'
+      ;;
+    (all-refs)
+      echo $'CTRL-O (open in browser) ╱ ALT-E (examine in editor)\n'
+      ble/contrib/integration:fzf-git/sub:refs 'cat'
+      ;;
+    (nobeep) ;;
+    (*) exit 1 ;;
+    esac
+  elif (($# > 1)); then
+    set -e
+
+    branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+    if [[ $branch = HEAD ]]; then
+      branch=$(git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD)
+    fi
+
+    # Only supports GitHub for now
+    case $1 in
+    (commit)
+      hash=$(grep -o "[a-f0-9]\{7,\}" <<< "$2")
+      path=/commit/$hash
+      ;;
+    (branch|remote-branch)
+      branch=$(sed 's/^[* ]*//' <<< "$2" | cut -d' ' -f1)
+      remote=$(git config branch."${branch}".remote || echo 'origin')
+      branch=${branch#$remote/}
+      path=/tree/$branch
+      ;;
+    (remote)
+      remote=$2
+      path=/tree/$branch
+      ;;
+    (file) path=/blob/$branch/$(git rev-parse --show-prefix)$2 ;;
+    (tag)  path=/releases/tag/$2 ;;
+    (*)    exit 1 ;;
+    esac
+
+    remote=${remote:-$(git config branch."${branch}".remote || echo 'origin')}
+    remote_url=$(git remote get-url "$remote" 2> /dev/null || echo "$remote")
+
+    if [[ $remote_url =~ ^git@ ]]; then
+      url=${remote_url%.git}
+      url=${url#git@}
+      url=https://${url/://}
+    elif [[ $remote_url =~ ^http ]]; then
+      url=${remote_url%.git}
+    fi
+
+    case $(uname -s) in
+    (Darwin) open "$url$path"     ;;
+    (*)      xdg-open "$url$path" ;;
+    esac
+  fi
+  exit 0
+fi
+
+#------------------------------------------------------------------------------
+
 ble-import contrib/integration/fzf-initialize
 
 [[ $- == *i* ]] || return 0
