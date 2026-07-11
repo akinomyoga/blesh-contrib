9edb318fccd03c935e40c4b82226e41dd45c1d77
SanŦeeg0 <142804744+SanTeeg0@users.noreply.github.com>
2026-07-11 10:35:23 +0300

    scheme: add "rosepine_{dawn,moon}" schemes (#31)
    Co-authored-by: Koichi Murase <myoga.murase@gmail.com>

---
+++ scheme/rosepine_dawn.bash
@@ -0,0 +1,27 @@
+# Scheme Inspired by Rosé Pine Dawn
+# https://rosepinetheme.com/palette/ingredients/#rose-pine-dawn
+
+ble-import contrib/scheme/catppuccin_mocha
+
+function ble/contrib/scheme:rosepine_dawn/initialize {
+  ble/contrib/scheme:catppuccin_mocha/initialize
+
+  ble/color/alias/set basic0   '#fffaf3' # Surface
+  ble/color/alias/set basic1   '#b4637a' # Love
+  ble/color/alias/set basic2   '#286983' # Pine
+  ble/color/alias/set basic3   '#ea9d34' # Gold
+  ble/color/alias/set basic4   '#907aa9' # Iris
+  ble/color/alias/set cursor   '#575279' # Text
+  ble/color/alias/set custom1  '#faf4ed' # Base
+  ble/color/alias/set custom2  '#ea9d34' # Gold
+  ble/color/alias/set custom3  '#56949f' # Foam
+  ble/color/alias/set custom4  '#907aa9' # Iris
+  ble/color/alias/set custom5  '#d7827e' # Rose
+  ble/color/alias/set custom6  '#56949f' # Foam
+  ble/color/alias/set custom7  '#797593' # Subtle
+  ble/color/alias/set custom8  '#907aa9' # Iris
+  ble/color/alias/set custom9  '#f2e9e1' # Overlay
+  ble/color/alias/set custom10 '#9893a5' # Muted
+
+  ble-face -s auto_complete 'fg=%custom10,italic'
+}
---
+++ scheme/rosepine_moon.bash
@@ -0,0 +1,27 @@
+# Scheme Inspired by Rosé Pine Moon
+# https://rosepinetheme.com/palette/ingredients/#rose-pine-moon
+
+ble-import contrib/scheme/catppuccin_mocha
+
+function ble/contrib/scheme:rosepine_moon/initialize {
+  ble/contrib/scheme:catppuccin_mocha/initialize
+
+  ble/color/alias/set basic0   '#2a273f' # Surface
+  ble/color/alias/set basic1   '#eb6f92' # Love
+  ble/color/alias/set basic2   '#3e8fb0' # Pine
+  ble/color/alias/set basic3   '#f6c177' # Gold
+  ble/color/alias/set basic4   '#c4a7e7' # Iris
+  ble/color/alias/set cursor   '#e0def4' # Text
+  ble/color/alias/set custom1  '#232136' # Base
+  ble/color/alias/set custom2  '#f6c177' # Gold
+  ble/color/alias/set custom3  '#9ccfd8' # Foam
+  ble/color/alias/set custom4  '#c4a7e7' # Iris
+  ble/color/alias/set custom5  '#ea9a97' # Rose
+  ble/color/alias/set custom6  '#9ccfd8' # Foam
+  ble/color/alias/set custom7  '#908caa' # Subtle
+  ble/color/alias/set custom8  '#c4a7e7' # Iris
+  ble/color/alias/set custom9  '#393552' # Overlay
+  ble/color/alias/set custom10 '#6e6a86' # Muted
+
+  ble-face -s auto_complete 'fg=%custom10,italic'
+}
