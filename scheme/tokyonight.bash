f95b210702a80a9f3ed69e14d28cba82c0bcd40d
Koichi Murase <myoga.murase@gmail.com>
2026-07-11 17:07:42 +0900

    scheme/tokyonight: remove extra quotations

--- scheme/tokyonight.bash
+++ scheme/tokyonight.bash
@@ -33,89 +33,89 @@ function ble/contrib/scheme:tokyonight/initialize {
   ble/color/alias/set custom12 '#41a6b5' # green2
 
   # highlighting related to editing
-  ble-face -s region                    "bg=%basic8,fg=%basic7"
-  ble-face -s region_target             "bg=%basic13,fg=%basic0"
-  ble-face -s region_match              "bg=%custom1,fg=%basic7"
-  ble-face -s region_insert             "fg=%basic4,bg=%custom1"
-  ble-face -s disabled                  "fg=%custom7"
-  ble-face -s overwrite_mode            "fg=%basic0,bg=%basic14"
+  ble-face -s region                    bg=%basic8,fg=%basic7
+  ble-face -s region_target             bg=%basic13,fg=%basic0
+  ble-face -s region_match              bg=%custom1,fg=%basic7
+  ble-face -s region_insert             fg=%basic4,bg=%custom1
+  ble-face -s disabled                  fg=%custom7
+  ble-face -s overwrite_mode            fg=%basic0,bg=%basic14
   ble-face -s vbell                     reverse
-  ble-face -s vbell_erase               "bg=%custom1"
-  ble-face -s vbell_flash               "fg=%basic2,reverse"
-  ble-face -s prompt_status_line        "fg=%basic7,bg=%custom9"
+  ble-face -s vbell_erase               bg=%custom1
+  ble-face -s vbell_flash               fg=%basic2,reverse
+  ble-face -s prompt_status_line        fg=%basic7,bg=%custom9
 
   # syntax highlighting
   ble-face -s syntax_default            none
-  ble-face -s syntax_command            "fg=%basic13"
-  ble-face -s syntax_quoted             "fg=%basic2"
-  ble-face -s syntax_quotation          "fg=%basic2,bold"
-  ble-face -s syntax_escape             "fg=%basic13"
-  ble-face -s syntax_expr               "fg=%custom3"
-  ble-face -s syntax_error              "bg=%basic1,fg=%basic7"
-  ble-face -s syntax_varname            "fg=%basic9"
+  ble-face -s syntax_command            fg=%basic13
+  ble-face -s syntax_quoted             fg=%basic2
+  ble-face -s syntax_quotation          fg=%basic2,bold
+  ble-face -s syntax_escape             fg=%basic13
+  ble-face -s syntax_expr               fg=%custom3
+  ble-face -s syntax_error              bg=%basic1,fg=%basic7
+  ble-face -s syntax_varname            fg=%basic9
   ble-face -s syntax_delimiter          bold
-  ble-face -s syntax_param_expansion    "fg=%custom10"    
-  ble-face -s syntax_history_expansion  "bg=%custom4,fg=%basic7"
-  ble-face -s syntax_function_name      "fg=%custom6,bold"
-  ble-face -s syntax_comment            "fg=%custom7"
-  ble-face -s syntax_glob               "fg=%basic9,bold"
-  ble-face -s syntax_brace              "fg=%custom8,bold"       
-  ble-face -s syntax_tilde              "fg=%basic5,bold"       
-  ble-face -s syntax_document           "fg=%custom9"
-  ble-face -s syntax_document_begin     "fg=%custom9,bold"
+  ble-face -s syntax_param_expansion    fg=%custom10    
+  ble-face -s syntax_history_expansion  bg=%custom4,fg=%basic7
+  ble-face -s syntax_function_name      fg=%custom6,bold
+  ble-face -s syntax_comment            fg=%custom7
+  ble-face -s syntax_glob               fg=%basic9,bold
+  ble-face -s syntax_brace              fg=%custom8,bold       
+  ble-face -s syntax_tilde              fg=%basic5,bold       
+  ble-face -s syntax_document           fg=%custom9
+  ble-face -s syntax_document_begin     fg=%custom9,bold
 
   # command highlighting
-  ble-face -s command_builtin_dot       "fg=%basic1,bold"
-  ble-face -s command_builtin           "fg=%basic1"
-  ble-face -s command_alias             "fg=%basic6"
-  ble-face -s command_function          "fg=%custom6"
-  ble-face -s command_file              "fg=%basic2"
-  ble-face -s command_keyword           "fg=%basic4"
-  ble-face -s command_jobs              "fg=%basic1"
-  ble-face -s command_directory         "fg=%custom3,underline"
-  ble-face -s command_suffix            "fg=%basic7,bg=%basic0"
-  ble-face -s command_suffix_new        "fg=%basic7,bg=%basic1"
+  ble-face -s command_builtin_dot       fg=%basic1,bold
+  ble-face -s command_builtin           fg=%basic1
+  ble-face -s command_alias             fg=%basic6
+  ble-face -s command_function          fg=%custom6
+  ble-face -s command_file              fg=%basic2
+  ble-face -s command_keyword           fg=%basic4
+  ble-face -s command_jobs              fg=%basic1
+  ble-face -s command_directory         fg=%custom3,underline
+  ble-face -s command_suffix            fg=%basic7,bg=%basic0
+  ble-face -s command_suffix_new        fg=%basic7,bg=%basic1
 
   # filename highlighting
-  ble-face -s filename_directory        "underline,fg=%custom3"
-  ble-face -s filename_directory_sticky "underline,fg=%basic7,bg=%custom1"
-  ble-face -s filename_link             "underline,fg=%basic6"
-  ble-face -s filename_orphan           "underline,fg=%basic0,bg=%basic3"
-  ble-face -s filename_executable       "underline,fg=%basic2"
-  ble-face -s filename_setuid           "underline,fg=%basic0,bg=%basic3"
-  ble-face -s filename_setgid           "underline,fg=%basic0,bg=%basic13"
+  ble-face -s filename_directory        underline,fg=%custom3
+  ble-face -s filename_directory_sticky underline,fg=%basic7,bg=%custom1
+  ble-face -s filename_link             underline,fg=%basic6
+  ble-face -s filename_orphan           underline,fg=%basic0,bg=%basic3
+  ble-face -s filename_executable       underline,fg=%basic2
+  ble-face -s filename_setuid           underline,fg=%basic0,bg=%basic3
+  ble-face -s filename_setgid           underline,fg=%basic0,bg=%basic13
   ble-face -s filename_other            underline
-  ble-face -s filename_socket           "underline,fg=%basic14,bg=%basic0"
-  ble-face -s filename_pipe             "underline,fg=%basic2,bg=%basic0"
-  ble-face -s filename_character        "underline,fg=%basic7,bg=%basic0"
-  ble-face -s filename_block            "underline,fg=%basic3,bg=%basic0"
-  ble-face -s filename_warning          "underline,fg=%basic1"
-  ble-face -s filename_url              "underline,fg=%basic4"
+  ble-face -s filename_socket           underline,fg=%basic14,bg=%basic0
+  ble-face -s filename_pipe             underline,fg=%basic2,bg=%basic0
+  ble-face -s filename_character        underline,fg=%basic7,bg=%basic0
+  ble-face -s filename_block            underline,fg=%basic3,bg=%basic0
+  ble-face -s filename_warning          underline,fg=%basic1
+  ble-face -s filename_url              underline,fg=%basic4
   ble-face -s filename_ls_colors        underline
 
   # varname highlighting
-  ble-face -s varname_array             "fg=%basic9,bold"
-  ble-face -s varname_empty             "fg=%custom2"      
-  ble-face -s varname_export            "fg=%basic13,bold"
-  ble-face -s varname_expr              "fg=%custom6,bold"
-  ble-face -s varname_hash              "fg=%custom12,bold"
-  ble-face -s varname_new               "fg=%custom11"     
-  ble-face -s varname_number            "fg=%basic4"
-  ble-face -s varname_readonly          "fg=%basic13"
-  ble-face -s varname_transform         "fg=%custom3,bold"
-  ble-face -s varname_unset             "fg=%custom7"
+  ble-face -s varname_array             fg=%basic9,bold
+  ble-face -s varname_empty             fg=%custom2      
+  ble-face -s varname_export            fg=%basic13,bold
+  ble-face -s varname_expr              fg=%custom6,bold
+  ble-face -s varname_hash              fg=%custom12,bold
+  ble-face -s varname_new               fg=%custom11     
+  ble-face -s varname_number            fg=%basic4
+  ble-face -s varname_readonly          fg=%basic13
+  ble-face -s varname_transform         fg=%custom3,bold
+  ble-face -s varname_unset             fg=%custom7
 
   # argument highlighting
-  ble-face -s argument_option           "fg=%basic6"
-  ble-face -s argument_error            "fg=%basic0,bg=%basic3"
+  ble-face -s argument_option           fg=%basic6
+  ble-face -s argument_error            fg=%basic0,bg=%basic3
 
   # highlighting for completions
-  ble-face -s auto_complete             "fg=%custom5,bg=%custom1" # approximate the default fg=238,bg=254
+  ble-face -s auto_complete             fg=%custom5,bg=%custom1 # approximate the default fg=238,bg=254
   ble-face -s menu_complete_match       bold
   ble-face -s menu_complete_selected    reverse
   ble-face -s menu_desc_default         none
   ble-face -s menu_desc_type            ref:syntax_delimiter
   ble-face -s menu_desc_quote           ref:syntax_quoted
   ble-face -s menu_filter_fixed         bold
-  ble-face -s menu_filter_input         "fg=%basic0,bg=%basic15"  # approximate the default fg=16,bg=229
+  ble-face -s menu_filter_input         fg=%basic0,bg=%basic15  # approximate the default fg=16,bg=229
 }
